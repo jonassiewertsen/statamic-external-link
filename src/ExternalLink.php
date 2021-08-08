964482125fa33971c0c7fbf1bc9606769a6288cd
Jonas Siewertsen <mail@jonassiewertsen.com>
2021-08-08 22:23:44 +0200

    I messed up. Fix my copy and paste error.

--- src/ExternalLink.php
+++ src/ExternalLink.php
@@ -46,57 +46,6 @@ class ExternalLink extends Fieldtype
     {
         return preg_replace('/^(http|https):\/\//i', '', $data);
     }
-    
-    <?php
-
-namespace Jonassiewertsen\ExternalLink;
-
-use Statamic\Fields\Fieldtype;
-
-class ExternalLink extends Fieldtype
-{
-    protected $icon = 'earth';
-
-    protected $categories = ['text'];
-
-    protected $regexPattern = '/^(http|https):\/\//i';
-
-    /**
-     * @return string
-     */
-    public static function title()
-    {
-        return 'External Link';
-    }
-
-    /**
-     * Adding the https:// before saving
-     *
-     * @param $data
-     *
-     * @return string
-     */
-    public function process($data)
-    {
-        if (! $data) {
-            return;
-        }
-
-        return 'https://' . $data;
-    }
-
-
-    /**
-     * Removing the https:// before displaying in field
-     *
-     * @param $data
-     *
-     * @return mixed
-     */
-    public function preProcess($data)
-    {
-        $data = preg_replace($this->regexPattern, '', $data);
-    }
 
     /**
      * Augment the value, which will return false in 
@@ -114,6 +63,4 @@ class ExternalLink extends Fieldtype
 
         return $value;
     }
-}
-
 }
